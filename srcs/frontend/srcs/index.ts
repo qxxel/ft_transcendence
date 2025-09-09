5f51e126850f840f76d685b7a08b404999ece013
agerbaud <agerbaud@student.42lyon.fr>
2025-09-09 20:19:46 +0200

    change test with html for create button

--- srcs/frontend/srcs/index.ts
+++ srcs/frontend/srcs/index.ts
@@ -6,20 +6,20 @@
  
 // document.body.textContent = greeter(user);
 
-console.log("Le front tourne !");
+// console.log("Le front tourne !");
 
-const canvas = document.createElement("canvas");
-canvas.width = 400;
-canvas.height = 400;
-document.body.appendChild(canvas);
+// const canvas = document.createElement("canvas");
+// canvas.width = 400;
+// canvas.height = 400;
+// document.body.appendChild(canvas);
 
-const ctx = canvas.getContext("2d");
-if (ctx) {
-  ctx.fillStyle = "blue";
-  ctx.fillRect(50, 50, 100, 100);
+// const ctx = canvas.getContext("2d");
+// if (ctx) {
+//   ctx.fillStyle = "blue";
+//   ctx.fillRect(50, 50, 100, 100);
 
-  ctx.fillStyle = "red";
-  ctx.beginPath();
-  ctx.arc(200, 200, 50, 0, Math.PI * 2);
-  ctx.fill();
-}
+//   ctx.fillStyle = "red";
+//   ctx.beginPath();
+//   ctx.arc(200, 200, 50, 0, Math.PI * 2);
+//   ctx.fill();
+// }
