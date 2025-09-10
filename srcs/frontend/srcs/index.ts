5d9f50b460aa931c6db0f6b36f23e1ab60fd087c
agerbaud <agerbaud@student.42lyon.fr>
2025-09-10 12:14:46 +0200

    change test

--- srcs/frontend/srcs/index.ts
+++ srcs/frontend/srcs/index.ts
@@ -23,3 +23,58 @@
 //   ctx.arc(200, 200, 50, 0, Math.PI * 2);
 //   ctx.fill();
 // }
+
+
+interface	Route {
+	path: string;
+	component: () => string;
+}
+
+class Router {
+	private routes: Route [] = [];
+
+	addRoute(path: string, component: () => string) {
+		this.routes.push({path, component});
+	}
+
+	navigate(path: string | null) {
+		history.pushState({}, '', path);
+    	this.render();
+	}
+
+	render() {
+		const currentPath = window.location.pathname;
+		const route = this.routes.find(r => r.path === currentPath);
+
+		if (route) {
+			const contentDiv = document.getElementById('app');
+      		if (contentDiv) {
+        		contentDiv.innerHTML = route.component();
+			}
+		}
+	}
+}
+
+// 1. Création du router
+const router = new Router();
+
+// 2. Définition des routes
+router.addRoute("/home", () => "<h1>Accueil</h1>");
+router.addRoute("/about", () => "<h1>À propos</h1>");
+
+// 3. QUAND la page change ? Quand on clique sur un lien !
+document.addEventListener('click', (e) => {
+  const target = e.target as HTMLAnchorElement;
+  if (target.tagName === 'A') {
+    e.preventDefault(); // Empêche le rechargement de page
+    router.navigate(target.getAttribute('href')!); // ← ICI la page change !
+  }
+});
+
+// 4. Gérer le bouton "retour" du navigateur
+window.addEventListener('popstate', () => {
+  router.render(); // Re-dessiner la page
+});
+
+// 5. Afficher la page initiale
+router.render();
