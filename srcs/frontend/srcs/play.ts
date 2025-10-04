3db7656839905c7730f991d7363129d477d77bd7
pip <mcurto@student.42lyon.fr>
2025-10-05 01:55:33 +0200

    circle in canvas.mp4

--- srcs/frontend/srcs/play.ts
+++ srcs/frontend/srcs/play.ts
@@ -1,7 +1,7 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas!.getContext("2d");
 
-// ctx!.beginPath();
-// ctx!.arc(100, 75, 50, 0, 2 * Math.PI);
-// ctx!.stroke();
+ctx!.beginPath();
+ctx!.arc(100, 75, 50, 0, 2 * Math.PI);
+ctx!.stroke();
 console.log("????S??S??QW?>?QW >R?QW R>?QW>? ")
