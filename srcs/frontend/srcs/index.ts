df5b9458164d62c1a60c78ba6e60bf48ca69080d
Axel Gerbaud <agerbaud@z4r8p7.42lyon.fr>
2025-10-09 18:17:27 +0200

    clean dockerfile and clean the ts code

--- srcs/frontend/srcs/index.ts
+++ srcs/frontend/srcs/index.ts
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   index.ts                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agerbaud <agerbaud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 13:58:03 by agerbaud          #+#    #+#             */
+/*   Updated: 2025/10/09 14:41:49 by agerbaud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 interface	Route {
 	path: string;
 	component: () => string | Promise<string>;
@@ -12,7 +24,7 @@ class Router {
 
 	navigate(path: string) {
 		history.pushState({}, '', path);
-    	this.render();
+		this.render();
 	}
 
 	async render() {
@@ -21,86 +33,86 @@ class Router {
 
 		if (route) {
 			const contentDiv = document.getElementById('app');
-      		if (contentDiv) {
+			if (contentDiv) {
 				const html = await route.component();
-        		contentDiv.innerHTML = html;
+				contentDiv.innerHTML = html;
 			}
 		}
 	}
 }
 
-// 1. Création du router
+/* ========================================================== */
+
 const router = new Router();
 
-// 1.bis Creer un menu
 const menu = `<nav>
 	<a href="/">Accueil</a> | 
 	<a href="/about">À propos</a> | 
 	<a href="/settings">Paramètres</a>
 	</nav>`;
 
+/* ========================================================== */
 
 async function loadHtml(path: string) {
-  const response = await fetch(path);
-  if (!response.ok) {
-	return `<h1>Erreur ${response.status}</h1><p>Impossible de charger ${path}</p>`;
-  }
-  return await response.text();
+	const response = await fetch(path);
+	if (!response.ok) {
+		return `<h1>Erreur ${response.status}</h1><p>Impossible de charger ${path}</p>`;
+	}
+	return await response.text();
 }
 
-
-	// 2. Définition des routes
-	// router.addRoute("/about", () => `${menu}`);
 router.addRoute("/about", async () => {
-  const html = await loadHtml("pages/about.html");
-  return menu + html;
+	const html = await loadHtml("pages/about.html");
+	return menu + html;
 });
-// router.addRoute("/settings", () => `${menu}<h1>Settings</h1><p class="name">Name</p><p class="name">Nickname</p>`);
+
 router.addRoute("/settings", async () => {
-  const html = await loadHtml("pages/settings.html");
-  return menu + html;
+	const html = await loadHtml("pages/settings.html");
+	return menu + html;
 });
-// router.addRoute("/rperrot", () => `${menu}<h1>The triathlete</h1><p>He's so bad at swiming !</p>`);
+
 router.addRoute("/rperrot", async () => {
-  const html = await loadHtml("pages/rperrot.html");
-  return menu + html;
+	const html = await loadHtml("pages/rperrot.html");
+	return menu + html;
 });
-// router.addRoute("/play", () => `${menu}<h1>Play</h1><a href=\"/localsolo\" class="txt">local solo</a> | <a href=\"/localmulti\" class="txt">local multiplayer</a>`);
+
 router.addRoute("/play", async () => {
-  const html = await loadHtml("pages/play.html");
-  return menu + html;
+	const html = await loadHtml("pages/play.html");
+	return menu + html;
 });
-// router.addRoute("/localmulti", () => `${menu}<h1>Local Multiplayer</h1>`);
+
 router.addRoute("/localmulti", async () => {
-  const html = await loadHtml("pages/localmulti.html");
-  return menu + html;
+	const html = await loadHtml("pages/localmulti.html");
+	return menu + html;
 });
-// router.addRoute("/localsolo", () => `${menu}<h1>Local Solo</h1>`);
+
 router.addRoute("/localsolo", async () => {
-  const html = await loadHtml("pages/localsolo.html");
-  return menu + html;
+	const html = await loadHtml("pages/localsolo.html");
+	return menu + html;
 });
-// router.addRoute("/", () => `${menu}<h1>Home pge</h1><a href="/play" class="txt">Play</a>`);
-// route racine "/"
+
 router.addRoute("/", async () => {
-  const html = await loadHtml("pages/home.html");
-  return menu + html;
+	const html = await loadHtml("pages/home.html");
+	return menu + html;
 });
 
+/* ========================================================== */
 
-// 3. QUAND la page change ? Quand on clique sur un lien !
 document.addEventListener('click', (e) => {
-  const target = e.target as HTMLAnchorElement;
-  if (target.tagName === 'A') {
-    e.preventDefault(); // Empêche le rechargement de page
-    router.navigate(target.getAttribute('href')!); // ← ICI la page change !
-  }
+	// const target = e.target as HTMLAnchorElement;
+	const target = e.target as HTMLElement;
+	
+	if (target.tagName === 'A') {
+		e.preventDefault();
+		router.navigate(target.getAttribute('href')!); // Changement de page
+	}
+
+	
 });
 
-// 4. Gérer le bouton "retour" du navigateur
+
 window.addEventListener('popstate', () => {
-  router.render(); // Re-dessiner la page
+	router.render();
 });
 
-// 5. Afficher la page initiale
 router.render();
