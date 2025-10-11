76cca5f2ff05089d0b146614e4652a666a2ca213
pip <mcurto@student.42lyon.fr>
2025-10-11 04:03:15 +0200

    +canvas at root

--- srcs/frontend/webpack.config.js
+++ srcs/frontend/webpack.config.js
@@ -4,9 +4,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const appDirectory = fs.realpathSync(process.cwd());
 
 module.exports = {
-    entry: path.resolve(appDirectory, "srcs/pong.ts"), //path to the main .ts file
+    entry: {
+        pong: path.resolve(appDirectory, "srcs/pong.ts"), //path to the main .ts file
+        index: path.resolve(appDirectory, "srcs/index.ts"),
+    },
     output: {
-        filename: "dist/pong.js", //name for the js file that is created/compiled in memory
+        filename: "[name].js", //name for the js file that is created/compiled in memory
         clean: true,
     },
     resolve: {
@@ -15,7 +18,7 @@ module.exports = {
     devServer: {
         host: "0.0.0.0",
         port: 8080, //port that we're using for local host (localhost:8080)
-        static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
+        static: path.resolve(appDirectory, "./"), //tells webpack to serve from the public folder
         hot: true,
         devMiddleware: {
             publicPath: "/",
