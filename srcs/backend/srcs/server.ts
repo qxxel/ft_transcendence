4870fbd1e154df7cde2462a2914efe1c90a850a7
Axel Gerbaud <agerbaud@z2r8p2.42lyon.fr>
2025-10-15 16:53:08 +0200

    add the definition of the db in the backend & add node_modules

--- srcs/backend/srcs/server.ts
+++ srcs/backend/srcs/server.ts
@@ -1,9 +1,16 @@
 import Fastify from 'fastify';
+import * as fs from 'fs';
+
+const sqlite3 = require('sqlite3').verbose();
 
 const fastify = Fastify({
 	logger: true
 });
 
+
+const db = new sqlite3.Database('/app/dist/db/mydatabase.db', (err: string) => {
+  if (err) console.error(err);
+});
 
 fastify.get('/', async (request, reply) => {
 	return { hello: 'world' };
@@ -12,8 +19,8 @@ fastify.get('/', async (request, reply) => {
 
 const start = async () => {
 	try {
-		await fastify.listen({ port: 9092 });
-		console.log(`Server started on http://localhost:9092`);
+		await fastify.listen({ port: 9090 });
+		console.log(`Server started on http://localhost:9090`);
 	} catch (err) {
 		fastify.log.error(err);
 		process.exit(1);
