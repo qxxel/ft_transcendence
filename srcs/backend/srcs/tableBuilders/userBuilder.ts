e901ea7fa6bc0d2eb3a45c3ec05ef833db9e8cac
Axel Gerbaud <agerbaud@z4r8p6.42lyon.fr>
2025-10-29 13:07:32 +0100

    add userTableBuilder function

--- srcs/backend/srcs/tableBuilders/userBuilder.ts
+++ srcs/backend/srcs/tableBuilders/userBuilder.ts
@@ -6,8 +6,27 @@
 /*   By: agerbaud <agerbaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 22:19:05 by agerbaud          #+#    #+#             */
-/*   Updated: 2025/10/28 22:19:33 by agerbaud         ###   ########.fr       */
+/*   Updated: 2025/10/29 12:53:46 by agerbaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// will build user table (called in userRepository)
+
+// WILL BUILD USER TABLE (CALLED IN 'userRepository')
+
+
+/* ====================== IMPORT ====================== */
+
+import { Database } from 'sqlite3'
+
+
+/* ====================== FUNCTIONS ====================== */
+
+export function	userTableBuilder(db: Database) {
+	db.exec(`CREATE TABLE IF NOT EXISTS usr_user (
+    	usr_spkuser INTEGER PRIMARY KEY AUTOINCREMENT,
+    	usr_cname TEXT NOT NULL UNIQUE,
+    	usr_cemail TEXT NOT NULL UNIQUE,
+    	usr_cpasswordhashed TEXT NOT NULL,
+    	usr_ielo INTEGER
+	);`);
+}
