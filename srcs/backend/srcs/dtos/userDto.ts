87964e87dd3163db126c72d633d2f215c1fcfeaf
Axel Gerbaud <agerbaud@z4r8p6.42lyon.fr>
2025-10-29 17:46:14 +0100

    add getters and remove useless interface

--- srcs/backend/srcs/dtos/userDto.ts
+++ srcs/backend/srcs/dtos/userDto.ts
@@ -6,7 +6,7 @@
 /*   By: agerbaud <agerbaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 22:18:37 by agerbaud          #+#    #+#             */
-/*   Updated: 2025/10/29 17:17:08 by agerbaud         ###   ########.fr       */
+/*   Updated: 2025/10/29 17:45:45 by agerbaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,20 @@
 import { validationResult, isValidName, isValidPwd } from '../utils/validation.js'
 
 
-/* ====================== INTERFACE ====================== */
-
-// export interface	userDto {
-// 	id: number,
-// 	username: string,
-// 	email: string,
-// 	passwordHashed: string,
-// 	elo: number
-// };
-
-
 /* ====================== CLASS ====================== */
 
 export class	userDto {
-	private id: number;
 	private	name: string;
 	private	email: string;
 	private	pwdHashed: string;
-	private	elo: number;
+	private	elo?: number;
 
 	constructor(row: any) {
-		this.id = row.usr_spkuser;
 		this.name = row.usr_cname;
 		this.email = row.usr_cemail;
 		this.pwdHashed = row.usr_cpasswordhashed;
-		this.elo = row.usr_ielo;
+		if (row.usr_ielo)
+			this.elo = row.usr_ielo;
 
 		var validation = this.isValid()
 		if (!validation.result)
@@ -56,5 +44,23 @@ export class	userDto {
 		const	errors = [nameResult.error, pwdResult.error].join(", ");
 
 		return { result: errors.length === 0, error: errors }
+	}
+
+
+	// GETTERS
+	getName(): string {
+		return this.name;
+	}
+
+	getEmail(): string {
+		return this.email;
+	}
+
+	getPwd(): string {
+		return this.pwdHashed;
+	}
+
+	getElo(): number {
+		return this.elo!;
 	}
 }
