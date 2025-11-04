52dc41eb9e964c4738a93daa818409a6073a8e14
Axel Gerbaud <agerbaud@z3r12p1.42lyon.fr>
2025-11-04 16:51:35 +0100

    change name of primary key in user table

--- srcs/backend/srcs/tableBuilders/userBuilder.ts
+++ srcs/backend/srcs/tableBuilders/userBuilder.ts
@@ -6,7 +6,7 @@
 /*   By: agerbaud <agerbaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 22:19:05 by agerbaud          #+#    #+#             */
-/*   Updated: 2025/11/03 23:06:00 by agerbaud         ###   ########.fr       */
+/*   Updated: 2025/11/04 15:56:56 by agerbaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ import { Database } from 'sqlite3'
 
 export function	userTableBuilder(db: Database) {
 	db.exec(`CREATE TABLE IF NOT EXISTS user (
-    	user INTEGER PRIMARY KEY AUTOINCREMENT,
+    	id INTEGER PRIMARY KEY AUTOINCREMENT,
     	username TEXT NOT NULL UNIQUE,
     	email TEXT NOT NULL UNIQUE,
     	password TEXT NOT NULL,
